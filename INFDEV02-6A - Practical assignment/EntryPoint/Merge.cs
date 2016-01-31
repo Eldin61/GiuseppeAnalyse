e00187907013d340a44649167ed773078b232344
eldin61 <eldin.331@gmail.com>
2016-01-31 18:50:14 +0100

    assignment 1 & 2 done

---
+++ INFDEV02-6A - Practical assignment/EntryPoint/Merge.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace EntryPoint
+{
+    class Merge
+    {
+        public List<Tuple<Vector2, float>> mergesort(List<Tuple<Vector2, float>> a)
+        {
+            // If list size is 0 (empty) or 1, consider it sorted and return ti
+            if (a.Count <= 1) return a;
+
+            // Else list size is > 1, so split the list into two sublists.
+            int midpoint = a.Count / 2;
+            List<Tuple<Vector2, float>> Left = new List<Tuple<Vector2, float>>();
+            List<Tuple<Vector2, float>> Right = new List<Tuple<Vector2, float>>();
+
+            for (int i = 0; i < midpoint; i++)
+                Left.Add(a[i]);
+            for (int i = midpoint; i < a.Count; i++)
+                Right.Add(a[i]);
+
+            // Recursively call MergeSort() to further split each sublist
+            // until sublist size is 1.
+            Left = mergesort(Left);
+            Right = mergesort(Right);
+
+            // Merge the sublists returned from prior calls to MergeSort()
+            // and return the resulting merged sublist.
+            return merge(Left, Right);
+        }   
+
+        public static List<Tuple<Vector2, float>> merge(List<Tuple<Vector2, float>> Left, List<Tuple<Vector2, float>> Right)
+        {
+            // While the sublist are not empty merge them repeatedly to produce new sublists 
+            // until there is only 1 sublist remaining. This will be the sorted list.
+            List<Tuple<Vector2, float>> result = new List<Tuple<Vector2, float>>();
+            while (Left.Count > 0 && Right.Count > 0)
+            {
+                if (Left[0].Item2 < Right[0].Item2)
+                {
+                    result.Add(Left[0]);
+                    Left.RemoveAt(0);
+                }
+                else
+                {
+                    result.Add(Right[0]);
+                    Right.RemoveAt(0);
+                }
+
+            }
+            while (Left.Count > 0)
+            {
+                result.Add(Left[0]);
+                Left.RemoveAt(0);
+            }
+            while (Right.Count > 0)
+            {
+                result.Add(Right[0]);
+                Right.RemoveAt(0);
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ INFDEV02-6A - Practical assignment/EntryPoint/Warshall.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace EntryPoint
+{
+    class Warshall
+    {
+        public List<Vector2> roadToBuilding = new List<Vector2>();
+
+        public int[,] CreateAdjMatrix(Dictionary<Vector2, int> adjList2, List<Tuple<Vector2, Vector2>> roads)
+        {
+            var adj = new int[adjList2.Count, adjList2.Count];
+
+            foreach (var road in roads)
+            {
+                adj[adjList2[road.Item1], adjList2[road.Item2]] = (int)Vector2.Distance(road.Item1, road.Item2);
+            }
+            for (var u = 0; u < adj.GetLength(0); u++) // Vertices
+            {
+                for (var v = 0; v < adj.GetLength(0); v++) //Edge
+                {
+                    if (adj[u, v] == 0 && u != v)
+                    {
+                        adj[u, v] = 5000000; //Infinity
+                    }
+                }
+            }
+            return adj;
+        }
+
+        private int[,] MatrixOfPredecessors(int[,] matrix)
+        {
+            var loopCount = matrix.GetLength(0);
+            var next = new int[loopCount, loopCount];
+
+            for (var u = 0; u < loopCount; u++) // Vertices
+            {
+                for (var v = 0; v < loopCount; v++) //Edge
+                {
+                    if (matrix[u, v] != 5000000 && u != v)
+                    {
+                        next[u, v] = u;
+                    }
+                    else
+                    {
+                        next[u, v] = -1;
+                    }
+                }
+            }
+            return next;
+        }
+
+
+    }
+}
+
